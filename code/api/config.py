0bd1956e83abba1093c665ba438ba6c79c058968
Felipe Santoro Alcantara <santoro.felipe@gmail.com>
2023-05-03 19:31:31 -0300

    .

--- code/api/config.py
+++ code/api/config.py
@@ -3,8 +3,8 @@ from sqlalchemy import create_engine
 DB_NAME = 'postgres'
 DB_USER = 'postgres'
 DB_PASSWORD = 'admin'
-DB_HOST = 'localhost'
-DB_PORT = '33010'
+DB_HOST = 'db_sprint_i'
+DB_PORT = '5432'
 
 DB_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
