141408aeb75f5ffd90ce4ee5e99dc122bc88074e
Felipe Santoro Alcantara <santoro.felipe@gmail.com>
2023-05-29 11:19:17 -0300

    date

--- code/api/model/dados_medidos.py
+++ code/api/model/dados_medidos.py
@@ -7,6 +7,7 @@ from model.base import Base
 
 class DadosMedidos(Base):
     __tablename__ = 'tb_dados_medidos'
+
     id = Column(Integer, primary_key=True)
     data = Column("data_medicao", DateTime, nullable=False)
     gordura = Column("porcen_gordura", Float)
@@ -19,8 +20,9 @@ class DadosMedidos(Base):
     peso = Column("peso", Float, nullable=False)
     cadastro_id = Column("dados_cadastro_id", Integer, ForeignKey('tb_dados_cadastro.id'), nullable=False)
 
-    def __init__(self, peso: float, gordura: float, gordura_visceral: int, idade: int, idade_corporal: int,
+    def __init__(self, data: DateTime, peso: float, gordura: float, gordura_visceral: int, idade: int, idade_corporal: int,
                  imc: float, met_basal: int, musculo: float, cadastro_id: int) -> None:
+        self.data = datetime.now()
         self.peso = peso
         self.gordura = gordura
         self.gordura_visceral = gordura_visceral
